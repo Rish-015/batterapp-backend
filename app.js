6afabe83c84d6c63e9507072436a0e72d0916be6
shaluk01 <krishnanshalu75@gmail.com>
2026-04-29 20:28:17 +0530

    updated app.js

--- app.js
+++ app.js
@@ -80,9 +80,8 @@ app.use((err, req, res, next) => {
 // =======================
 // SERVER START
 // =======================
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
